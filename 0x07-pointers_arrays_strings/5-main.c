01570fe2eb34c49600de2a83f0aff366f241c3aa
abood-alramadeen20 <abd2020yy2@gmail.com>
2023-07-03 18:19:39 +0300

    updated

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -16,4 +15,3 @@ int main(void)
     printf("%s\n", t);
     return (0);
 }
-
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -22,3 +22,6 @@ char *_strstr(char *haystack, char *needle)
 			return (haystack);
 	}
 
+	return (0);
+}
+
